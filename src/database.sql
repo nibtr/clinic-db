285566e19e79ce2fa3cdc4530d65a78bb4b47773
nhman2002 <nhman2002@gmail.com>
2023-07-27 14:55:38 +0700

    update on Hieus DATATYPE
    update on Hieus DATATYPE

--- src/database.sql
+++ src/database.sql
@@ -3,25 +3,26 @@ go
 use Testing_DB
 go
 
-BEGIN TRY
+--BEGIN TRY
 
-BEGIN TRAN;
+--BEGIN TRAN;
 CREATE TABLE [dbo].[Account] (
     [id] INT NOT NULL IDENTITY(1,1),
-    [username] CHAR(10) NOT NULL,
+    [username] CHAR(10) UNIQUE NOT NULL,
     [password] CHAR(60) NOT NULL,
-    [email] VARCHAR(50),
+    [email] VARCHAR(50) UNIQUE,
+    [role] CHAR(3) NOT NULL,
+    [personelId] INT NOT NULL,
     CONSTRAINT [id] PRIMARY KEY CLUSTERED ([id])
 );
 
 CREATE TABLE [dbo].[Personel] (
     [id] INT NOT NULL IDENTITY(1,1),
-    [nationalId] CHAR(21) NOT NULL,
+    [nationalId] CHAR(12) UNIQUE NOT NULL,
     [name] NVARCHAR(50) NOT NULL,
     [dob] DATE,
     [gender] CHAR(1),
-	[phone]	CHAR(10),
-	[accountId] INT,
+	[phone]	CHAR(10) UNIQUE,
 	CONSTRAINT [Personel_pkey] PRIMARY KEY CLUSTERED ([id])
 );
 
@@ -35,32 +36,32 @@ CREATE TABLE [dbo].[Patient] (
 
 CREATE TABLE [dbo].[PaymentRecord] (
     [id] INT NOT NULL IDENTITY(1,1),
-    [date] DATE,
-    [total] INT,
-    [amountPaid] INT,
-	[change] INT,
+    [date] DATE NOT NULL,
+    [total] INT NOT NULL,
+    [paid] INT DEFAULT 0,
+	[change] INT DEFAULT 0,
 	[method] CHAR(1),
-	[patientId] INT,
-	[treatmentSessionId] INT,
+	[patientId] INT NOT NULL,
+	[treatmentSessionId] INT NOT NULL,
 	CONSTRAINT [PaymentRecord_pkey] PRIMARY KEY CLUSTERED ([id])
 );	
 
 CREATE TABLE [dbo].[Session] (
     [id] INT NOT NULL IDENTITY(1,1),
-    [time] DATETIME2,
+    [time] DATETIME2 NOT NULL,
     [note] NVARCHAR(1000),
-    [status] CHAR(3),
-	[patientId] INT,
+    [status] CHAR(6) DEFAULT 'do',
+	[patientId] INT NOT NULL,
 	[assistantId] INT,
-	[dentistId] INT,
-	[roomId] INT,
+	[dentistId] INT NOT NULL,
+	[roomId] INT NOT NULL,
 	CONSTRAINT [Session_pkey] PRIMARY KEY CLUSTERED ([id])
 );	
 
 CREATE TABLE [dbo].[Room] (
     [id] INT NOT NULL IDENTITY(1,1),
-    [code] CHAR(6),
-    [name] VARCHAR(50),
+    [code] CHAR(6) UNIQUE,
+    [name] VARCHAR(50) NOT NULL,
 	[amount] INT,
 	CONSTRAINT [Room_pkey] PRIMARY KEY CLUSTERED ([id])
 );	
@@ -69,64 +70,64 @@ CREATE TABLE [dbo].[TreatmentSession] (
     [id] INT NOT NULL,
     [health_note] NVARCHAR(1000),
     [description] NVARCHAR(1000),
-	[categoryId] INT,
+	[categoryId] INT NOT NULL,
 	CONSTRAINT [TreatmentSession_pkey] PRIMARY KEY CLUSTERED ([id])
 );	
 
 CREATE TABLE [dbo].[Procedure] (
     [id] INT NOT NULL IDENTITY(1,1),
-    [code] CHAR(3),
-    [name] NVARCHAR(50),
+    [code] CHAR(3) UNIQUE,
+    [name] NVARCHAR(50) NOT NULL,
     [description] NVARCHAR(500),
-    [fee] INT,
-	[categoryId] INT,
+    [fee] INT NOT NULL,
+	[categoryId] INT NOT NULL,
 	CONSTRAINT [Procedure_pkey] PRIMARY KEY CLUSTERED ([id])
 );	
 
 CREATE TABLE [dbo].[Category] (
     [id] INT NOT NULL IDENTITY(1,1),
-    [code] CHAR(3),
-    [name] NVARCHAR(50),
+    [code] CHAR(3) UNIQUE,
+    [name] NVARCHAR(50) NOT NULL,
     [description] NVARCHAR(255),
 	CONSTRAINT [Category_pkey] PRIMARY KEY CLUSTERED ([id])
 );	
 
 CREATE TABLE [dbo].[Drug] (
     [id] INT NOT NULL IDENTITY(1,1),
-    [code] CHAR(17),
-    [name] NVARCHAR(50),
+    [code] CHAR(17) UNIQUE,
+    [name] NVARCHAR(50) NOT NULL,
     [description] NVARCHAR(255),
-    [price] INT,
+    [price] INT NOT NULL,
 	CONSTRAINT [Drug_pkey] PRIMARY KEY CLUSTERED ([id])
 );
 
 CREATE TABLE [dbo].[Tooth] (
     [id] INT NOT NULL IDENTITY(1,1),
-    [type] CHAR(1),
-    [description] NVARCHAR(255),
+    [type] CHAR(1) UNIQUE,
+    [name] NVARCHAR(50) NOT NULL,
 	CONSTRAINT [Tooth_pkey] PRIMARY KEY CLUSTERED ([type])
 );	
 
 CREATE TABLE [dbo].[Prescription] (
-    [note] NVARCHAR(1000),
-	[drugId] INT,
-	[sessionId] INT,
-	CONSTRAINT [Prescription_pkey] PRIMARY KEY CLUSTERED ([drugId],[sessionId])
+    [note] NVARCHAR(500),
+	[drugId] INT NOT NULL,
+	[treatmentSessionId] INT NOT NULL,
+	CONSTRAINT [Prescription_pkey] PRIMARY KEY CLUSTERED ([drugId],[treatmentSessionId])
 );	
 
 CREATE TABLE [dbo].[AppointmentRequest] (
     [id] INT NOT NULL IDENTITY(1,1),
-    [appointmentTime] DATETIME2,
-    [requestTime] DATETIME2,
+    [appointmentTime] DATETIME2 NOT NULL,
+    [requestTime] DATETIME2 NOT NULL,
     [note] NVARCHAR(255),
-    [patientName] NVARCHAR(50),
-    [patientPhone] CHAR(10),
+    [patientName] NVARCHAR(50) NOT NULL,
+    [patientPhone] CHAR(10) NOT NULL,
 	CONSTRAINT [AppointmentRequest_pkey] PRIMARY KEY CLUSTERED ([id])
 );	
 
 CREATE TABLE [dbo].[Schedule] (
-    [dayId] INT,
-    [dentistId] INT,
+    [dayId] INT NOT NULL,
+    [dentistId] INT NOT NULL,
 	CONSTRAINT [Schedule_pkey] PRIMARY KEY CLUSTERED ([dayId],[dentistId])
 );	
 
@@ -164,14 +165,14 @@ CREATE TABLE [dbo].[ExaminationSession] (
 
 CREATE TABLE [dbo].[ReExaminationSession] (
     [id] INT,
-	[relatedExamination] INT,
+	[relatedExamination] INT NOT NULL,
 	CONSTRAINT [ReExaminationSession_pkey] PRIMARY KEY CLUSTERED ([id],[relatedExamination])
 );	
 
 CREATE TABLE [dbo].[Day] (
     [id] INT NOT NULL IDENTITY(1,1),
 	[dentistId] INT,
-	[day] VARCHAR(50),
+	[day] CHAR(3) UNIQUE,
 	CONSTRAINT [Day_pkey] PRIMARY KEY CLUSTERED ([id])
 );	
 
@@ -179,32 +180,32 @@ CREATE TABLE [dbo].[Day] (
 -- Constraint in table Patient
 ALTER TABLE [dbo].[Patient] ADD CONSTRAINT [FK_Patient_Personel] FOREIGN KEY ([id]) REFERENCES [dbo].[Personel]([id]) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- Constraint in table Personel
-ALTER TABLE [dbo].[Personel] ADD CONSTRAINT [FK_Personel_Account] FOREIGN KEY ([accountId]) REFERENCES [dbo].[Account]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
+-- Constraint in table Account
+ALTER TABLE [dbo].[Account] ADD CONSTRAINT [FK_Account_Personel] FOREIGN KEY ([personelId]) REFERENCES [dbo].[Personel]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 
 -- Constraint in table Staff
 AlTER TABLE [dbo].[Staff] ADD CONSTRAINT [FK_Staff_Personel] FOREIGN KEY ([id]) REFERENCES [dbo].[Personel]([id]) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- Constraint in table Payment Record
-AlTER TABLE [dbo].[PaymentRecord] ADD CONSTRAINT [FK_PaymentRecord_Patient] FOREIGN KEY ([patientId]) REFERENCES [dbo].[Patient]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
-AlTER TABLE [dbo].[PaymentRecord] ADD CONSTRAINT [FK_PaymentRecord_TreatmentSession] FOREIGN KEY ([treatmentSessionId]) REFERENCES [dbo].[TreatmentSession]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
+AlTER TABLE [dbo].[PaymentRecord] ADD CONSTRAINT [FK_PaymentRecord_Patient] FOREIGN KEY ([patientId]) REFERENCES [dbo].[Patient]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+AlTER TABLE [dbo].[PaymentRecord] ADD CONSTRAINT [FK_PaymentRecord_TreatmentSession] FOREIGN KEY ([treatmentSessionId]) REFERENCES [dbo].[TreatmentSession]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- Constraint in table Session
-AlTER TABLE [dbo].[Session] ADD CONSTRAINT [FK_Session_Room] FOREIGN KEY ([roomId]) REFERENCES [dbo].[Room]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
-AlTER TABLE [dbo].[Session] ADD CONSTRAINT [FK_Session_Patient] FOREIGN KEY ([patientId]) REFERENCES [dbo].[Patient]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
+AlTER TABLE [dbo].[Session] ADD CONSTRAINT [FK_Session_Room] FOREIGN KEY ([roomId]) REFERENCES [dbo].[Room]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+AlTER TABLE [dbo].[Session] ADD CONSTRAINT [FK_Session_Patient] FOREIGN KEY ([patientId]) REFERENCES [dbo].[Patient]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
 AlTER TABLE [dbo].[Session] ADD CONSTRAINT [FK_Session_Assistant] FOREIGN KEY ([assistantId]) REFERENCES [dbo].[Assistant]([assistantId]) ON DELETE SET NULL ON UPDATE CASCADE;
-AlTER TABLE [dbo].[Session] ADD CONSTRAINT [FK_Session_Dentist] FOREIGN KEY ([dentistId]) REFERENCES [dbo].[Dentist]([dentistId]) ON DELETE SET NULL ON UPDATE CASCADE;
+AlTER TABLE [dbo].[Session] ADD CONSTRAINT [FK_Session_Dentist] FOREIGN KEY ([dentistId]) REFERENCES [dbo].[Dentist]([dentistId]) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- Constraint in table Treatment Session
 AlTER TABLE [dbo].[TreatmentSession] ADD CONSTRAINT [FK_TreatmentSession_Session] FOREIGN KEY ([id]) REFERENCES [dbo].[Session]([id]) ON DELETE NO ACTION;
 AlTER TABLE [dbo].[TreatmentSession] ADD CONSTRAINT [FK_TreatmentSession_Category] FOREIGN KEY ([categoryId]) REFERENCES [dbo].[Category]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- Constraint in table Procedure
-AlTER TABLE [dbo].[Procedure] ADD CONSTRAINT [FK_Procedure_Category] FOREIGN KEY ([categoryId]) REFERENCES [dbo].[Category]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
+AlTER TABLE [dbo].[Procedure] ADD CONSTRAINT [FK_Procedure_Category] FOREIGN KEY ([categoryId]) REFERENCES [dbo].[Category]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- Constraint in table Prescription
-AlTER TABLE [dbo].[Prescription] ADD CONSTRAINT [FK_Prescription_TreatmentSession] FOREIGN KEY ([sessionId]) REFERENCES [dbo].[TreatmentSession]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+AlTER TABLE [dbo].[Prescription] ADD CONSTRAINT [FK_Prescription_TreatmentSession] FOREIGN KEY ([treatmentSessionId]) REFERENCES [dbo].[TreatmentSession]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
 AlTER TABLE [dbo].[Prescription] ADD CONSTRAINT [FK_Prescription_Drug] FOREIGN KEY ([drugId]) REFERENCES [dbo].[Drug]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 -- Constraint in table Tooth Session
@@ -225,6 +226,7 @@ AlTER TABLE [dbo].[Assistant] ADD CONSTRAINT [FK_Assistant_Dentist] FOREIGN KEY 
 -- Constraint in table Dentist
 AlTER TABLE [dbo].[Dentist] ADD CONSTRAINT [FK_Dentist_Personel] FOREIGN KEY ([dentistId]) REFERENCES [dbo].[Personel]([id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
 AlTER TABLE [dbo].[Dentist] ADD CONSTRAINT [FK_Dentist_Schedule] FOREIGN KEY ([dayID],[scheduleId]) REFERENCES [dbo].[Schedule]([dayId],[dentistId]) ON DELETE SET NULL ON UPDATE CASCADE;
+
 -- Constraint in table Day
 AlTER TABLE [dbo].[Day] ADD CONSTRAINT [FK_Day_Schedule] FOREIGN KEY ([id],[dentistId]) REFERENCES [dbo].[Schedule]([dayId],[dentistId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
@@ -240,8 +242,8 @@ END CATCH;
 
 
 --Use for drop the database
---use master
---go
---alter database Testing_DB set single_user with rollback immediate
---drop database Testing_DB
+use master
+go
+alter database Testing_DB set single_user with rollback immediate
+drop database Testing_DB
 
