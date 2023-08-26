14f98ef2b61cb4e4daa9f7c2875382c8149810de
nhman2002 <nhman2002@gmail.com>
2023-08-26 11:38:41 +0700

    WIP STA procedure

--- src/procedure.sql
+++ src/procedure.sql
@@ -46,17 +46,17 @@ CREATE PROCEDURE delAppoint					--STA2
 @appointmentID INT
 AS
 BEGIN
-		BEGIN TRY
-			BEGIN TRAN
-			BEGIN
-				DELETE FROM [dbo].[AppointmentRequest] 
-				WHERE [dbo].[AppointmentRequest].id = @appointmentID
-			END
-			COMMIT TRAN
-		END TRY
-		BEGIN CATCH
-			ROLLBACK TRAN
-		END CATCH
+	BEGIN TRY
+		BEGIN TRAN
+		BEGIN
+			DELETE FROM [dbo].[AppointmentRequest] 
+			WHERE [dbo].[AppointmentRequest].id = @appointmentID
+		END
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
 END
 GO
 
@@ -91,23 +91,23 @@ CREATE PROCEDURE createNewExaminationSesion				--STA4
 @dentistID INT
 AS
 BEGIN
-		BEGIN TRY
-			BEGIN TRAN
-			DECLARE @PatientID INT;
-			DECLARE @SessionID INT;
-			IF EXISTS (SELECT [dbo].[Patient].id, [dbo].[Patient].name FROM [dbo].[Patient] WHERE [dbo].[Patient].phone = @patientPhone)
-			BEGIN
-				SELECT @PatientID = id FROM [dbo].[Patient] WHERE [dbo].[Patient].phone = @patientPhone;
-				DECLARE @InsertedIDs TABLE (ID INT);
-				INSERT INTO [dbo].[Session](time, patientID, note, type, roomID, dentistID) OUTPUT inserted.id INTO @InsertedIDs VALUES (GETDATE(), @PatientID, @note, 'EXA', @roomID, @dentistID);
-				SELECT @SessionID = ID FROM @InsertedIDs;
-				INSERT INTO [dbo].[ExaminationSession](id) VALUES (@SessionID)
-			END
-			COMMIT TRAN
-		END TRY
-		BEGIN CATCH
-			ROLLBACK TRAN
-		END CATCH
+	BEGIN TRY
+		BEGIN TRAN
+		DECLARE @PatientID INT;
+		DECLARE @SessionID INT;
+		IF EXISTS (SELECT [dbo].[Patient].id, [dbo].[Patient].name FROM [dbo].[Patient] WHERE [dbo].[Patient].phone = @patientPhone)
+		BEGIN
+			SELECT @PatientID = id FROM [dbo].[Patient] WHERE [dbo].[Patient].phone = @patientPhone;
+			DECLARE @InsertedIDs TABLE (ID INT);
+			INSERT INTO [dbo].[Session](time, patientID, note, type, roomID, dentistID) OUTPUT inserted.id INTO @InsertedIDs VALUES (GETDATE(), @PatientID, @note, 'EXA', @roomID, @dentistID);
+			SELECT @SessionID = ID FROM @InsertedIDs;
+			INSERT INTO [dbo].[ExaminationSession](id) VALUES (@SessionID)
+		END
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
 END
 GO
 
@@ -120,67 +120,58 @@ CREATE PROCEDURE createNewReExaminationSession			--STA5
 @assistantID INT
 AS 
 BEGIN
-		BEGIN TRY
-			BEGIN TRAN
-			DECLARE @PatientID INT;
-			DECLARE @examSessionID INT;
-			DECLARE @SessionID INT;
-			DECLARE @time DATETIME2;
-			SELECT top 1 @time = appointmentTime FROM AppointmentRequest WHERE dbo.AppointmentRequest.patientPhone = @patientPhone AND  appointmentTime < GETDATE() ORDER BY id DESC 
-			SELECT @PatientID = id FROM [dbo].[Patient] WHERE [dbo].[Patient].phone = @patientPhone
-			IF @PatientID IS NOT NULL 
-			BEGIN
-				DECLARE @InsertedIDs TABLE (ID INT);
-				IF EXISTS (SELECT [dbo].[Personnel].id FROM [dbo].[Personnel] WHERE [dbo].[Personnel].id = @assistantID AND [dbo].[Personnel].type = 'AST')
-					BEGIN
-						SELECT @examSessionID = MAX(id) FROM [dbo].[Session] WHERE [dbo].[Session].patientID = @PatientID AND [dbo].[Session].type = 'EXA'
-						INSERT INTO [dbo].[Session](time, patientID, note, type, roomID, dentistID, assistantID) OUTPUT inserted.id INTO @InsertedIDs VALUES (@time, @PatientID, @note, 'REX', @roomID, @dentistID, @assistantID);
-						SELECT @SessionID = ID FROM @InsertedIDs;
-					END
-					ELSE
-					BEGIN
-						SELECT @examSessionID = MAX(id) FROM [dbo].[Session] WHERE [dbo].[Session].patientID = @PatientID AND [dbo].[Session].type = 'EXA'
-						INSERT INTO [dbo].[Session](time, patientID, note, type, roomID, dentistID) OUTPUT inserted.id INTO @InsertedIDs VALUES (@time, @PatientID, @note, 'REX', @roomID, @dentistID);
-						SELECT @SessionID = ID FROM @InsertedIDs;
-					END
-				INSERT INTO [dbo].[ReExaminationSession] (id,relatedExaminationID) VALUES (@SessionID, @examSessionID);
-			END
-			COMMIT TRAN
-		END TRY
-		BEGIN CATCH
-			print('123')
-			ROLLBACK TRAN
-		END CATCH
+	BEGIN TRY
+		BEGIN TRAN
+		DECLARE @PatientID INT;
+		DECLARE @examSessionID INT;
+		DECLARE @SessionID INT;
+		DECLARE @time DATETIME2;
+		SELECT top 1 @time = appointmentTime FROM AppointmentRequest WHERE dbo.AppointmentRequest.patientPhone = @patientPhone AND  appointmentTime < GETDATE() ORDER BY id DESC 
+		SELECT @PatientID = id FROM [dbo].[Patient] WHERE [dbo].[Patient].phone = @patientPhone
+		IF @PatientID IS NOT NULL 
+		BEGIN
+			DECLARE @InsertedIDs TABLE (ID INT);
+			IF EXISTS (SELECT [dbo].[Personnel].id FROM [dbo].[Personnel] WHERE [dbo].[Personnel].id = @assistantID AND [dbo].[Personnel].type = 'AST')
+				BEGIN
+					SELECT @examSessionID = MAX(id) FROM [dbo].[Session] WHERE [dbo].[Session].patientID = @PatientID AND [dbo].[Session].type = 'EXA'
+					INSERT INTO [dbo].[Session](time, patientID, note, type, roomID, dentistID, assistantID) OUTPUT inserted.id INTO @InsertedIDs VALUES (@time, @PatientID, @note, 'REX', @roomID, @dentistID, @assistantID);
+					SELECT @SessionID = ID FROM @InsertedIDs;
+				END
+				ELSE
+				BEGIN
+					SELECT @examSessionID = MAX(id) FROM [dbo].[Session] WHERE [dbo].[Session].patientID = @PatientID AND [dbo].[Session].type = 'EXA'
+					INSERT INTO [dbo].[Session](time, patientID, note, type, roomID, dentistID) OUTPUT inserted.id INTO @InsertedIDs VALUES (@time, @PatientID, @note, 'REX', @roomID, @dentistID);
+					SELECT @SessionID = ID FROM @InsertedIDs;
+				END
+			INSERT INTO [dbo].[ReExaminationSession] (id,relatedExaminationID) VALUES (@SessionID, @examSessionID);
+		END
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		print('ERROR')
+		ROLLBACK TRAN
+	END CATCH
 END
 GO
 
 CREATE PROCEDURE viewAvailableDentist			--STA7			-- FIX THIS PROC
-@appointmentID INT
+@day INT
 AS 
 BEGIN
-		BEGIN TRY
-			BEGIN TRAN
-			
-			DECLARE @SessionID INT;
-			DECLARE @Time DATETIME2;
-			IF EXISTS (SELECT [dbo].[AppointmentRequest].id FROM [dbo].[AppointmentRequest] WHERE [dbo].[AppointmentRequest].id = @appointmentID)
-			BEGIN
-				SELECT  @Time = appointmentTime FROM [dbo].[AppointmentRequest] WHERE [dbo].[AppointmentRequest].id = @appointmentID;
-				DECLARE @EndTime DATETIME2;
- 	            SET @EndTime = DATEADD(MINUTE, 30, @Time);
-				SELECT P.id AS dentistID, P.name AS dentistName
-            	FROM [dbo].[Personnel] P
-            	WHERE P.type = 'DEN' AND P.id NOT IN (
-                	SELECT DISTINCT S.[dentistID]
-                	FROM [dbo].[Session] S
-                	WHERE S.[time] >= @Time AND S.[time] <= @EndTime
-            	);
-			END
-			COMMIT TRAN
-		END TRY
-		BEGIN CATCH
-			ROLLBACK TRAN
-		END CATCH
+	BEGIN TRY
+		BEGIN TRAN
+		DECLARE @SessionID INT;
+		DECLARE @Time DATETIME2;
+		IF (@day IN (1,2,3,4,5,6,7))
+		BEGIN
+			SELECT *
+			FROM [dbo].[SCHEDULE] S JOIN [dbo].[Personnel] P ON P.id = S.dentistID WHERE S.dayID = @day;
+		END
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
 END
 GO
 
@@ -194,18 +185,99 @@ CREATE PROCEDURE createNewPayment			--STA14
 @paid INT
 AS 
 BEGIN
+	BEGIN TRY
+		BEGIN TRAN
+		DECLARE @PatientID INT;
+		SELECT @PatientID = [dbo].[Patient].id FROM [dbo].[Patient] WHERE [dbo].[Patient].phone = @patientPhone
+		IF @PatientID IS NOT NULL
+		BEGIN
+			INSERT INTO [dbo].[PaymentRecord](date, total, paid, change, patientID) VALUES (CONVERT(DATE, GETDATE()), @total, @paid, @change, @PatientID)
+		END
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE updatePayment				--STA15
+@patientPhone CHAR(10),
+@paid INT,
+@change INT,
+@method CHAR(1),
+@date DATETIME2
+AS 
+BEGIN
+	BEGIN TRY
+		BEGIN TRAN
+		DECLARE @PatientID INT;
+		DECLARE @TotalFee INT;
+		SELECT @PatientID = id FROM [dbo].[Patient] WHERE [dbo].[Patient].phone = @patientPhone
+		IF @PatientID IS NOT NULL
+		BEGIN
+			UPDATE [dbo].[PaymentRecord]
+			SET [dbo].[PaymentRecord].[paid] = @paid,
+			[dbo].[PaymentRecord].[change] = @change,
+			[dbo].[PaymentRecord].[method] = @method,
+			[dbo].[PaymentRecord].[date] = CONVERT(DATE, GETDATE())
+			WHERE [patientID] = @PatientID AND [date] = @date
+		END
+		ELSE
+		BEGIN
+			PRINT('ERROR')
+		END
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE viewPaymentRecord			--STA17
+@patientID INT
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRAN
+		IF EXISTS (SELECT [dbo].[PaymentRecord].id, [dbo].[PaymentRecord].date FROM [dbo].[PaymentRecord] WHERE [dbo].[PaymentRecord].patientID = @patientID)
+		BEGIN
+			SELECT * FROM [dbo].[PaymentRecord] WHERE [dbo].[PaymentRecord].patientID = @patientID
+		END
+		COMMIT TRAN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE createNewSession				--STA24
+@patientPhone CHAR(10),
+@note VARCHAR(1000),
+@room INT,
+@dentistID INT,
+@assistantID INT
+AS
+BEGIN
+	BEGIN TRAN
 		BEGIN TRY
-			BEGIN TRAN
 			DECLARE @PatientID INT;
-			IF EXISTS (SELECT [dbo].[Patient].id, [dbo].[Patient].name FROM [dbo].[Patient] WHERE [dbo].[Patient].phone = @patientPhone)
+			DECLARE @SessionID INT;
+			SELECT @PatientID = id FROM [dbo].[Patient] WHERE [dbo].[Patient].phone = @patientPhone;
+			IF @PatientID IS NOT NULL
 			BEGIN
-				SELECT @PatientID = id FROM [dbo].[Patient] WHERE [dbo].[Patient].phone = @patientPhone;
-				INSERT INTO [dbo].[PaymentRecord](date, total, paid, change, patientID) VALUES (GETDATE(), @total, @paid, @change, @PatientID)
+				DECLARE @InsertedIDs TABLE (ID INT);
+				INSERT INTO [dbo].[Session](time, patientID, note, type, status, dentistID, assistantID, roomID) OUTPUT inserted.id INTO @InsertedIDs VALUES (CONVERT(DATETIME2,GETDATE()), @PatientID, @note, 'TRE','SCH', @dentistID, @assistantID, @room);
+				SELECT @SessionID = ID FROM @InsertedIDs;
+				INSERT INTO [dbo].[ExaminationSession] (id) VALUES (@SessionID)
 			END
 			COMMIT TRAN
 		END TRY
 		BEGIN CATCH
 			ROLLBACK TRAN
 		END CATCH
+	COMMIT TRAN
 END
 GO
